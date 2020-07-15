7b2782b08c6fb250c75cadd60933bceaf0c48f1a
Zhe Sun <sunny728415@gmail.com>
2020-07-15 10:30:27 -0500

    Completed LeapYear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,15 @@ public class LeapYear {
         }
     }
 
+    public static boolean isLeapYear(int year){
+        boolean Lyear = false;
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)){
+            Lyear = true;
+        }
+        return Lyear;
+
+    }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
